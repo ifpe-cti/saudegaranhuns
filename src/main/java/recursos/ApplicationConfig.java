2f8797becab661a75fdb66bd32724507ec865841
Junio <junio.araujo01@yahoo.com>
2017-11-21 21:53:05 -0300

    Adicionando o recurso de Consulta

---
+++ src/main/java/recursos/ApplicationConfig.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recursos;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Jose Junio
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method. It is automatically
+     * populated with all resources defined in the project. If required, comment
+     * out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+        resources.add(recursos.ConsultasResource.class);
+    }
+
+}
