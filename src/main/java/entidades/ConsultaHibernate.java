a77407e6da04dff5030b55cdd681c69007eaea5b
Junio <junio.araujo01@yahoo.com>
2017-11-24 00:23:24 -0300

    Criação de service generico para correção de bug

---
+++ src/main/java/entidades/ConsultaHibernate.java
@@ -0,0 +1,39 @@
+package entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Jose Junio
+ */
+public class ConsultaHibernate {
+
+    private List<Consulta> consultas;
+
+    public ConsultaHibernate() {
+        this.consultas = new ArrayList<>();
+    }
+
+    public ConsultaHibernate(List<Consulta> consultas) {
+        this.consultas = consultas;
+    }
+
+    public List<Consulta> getConsultas() {
+        return consultas;
+    }
+
+    public void setConsultas(List<Consulta> consultas) {
+        this.consultas = consultas;
+    }
+
+    public void salvar(Consulta consulta) {
+        this.consultas.add(consulta);
+    }
+
+}
--- src/main/java/recursos/ApplicationConfig.java
+++ src/main/java/recursos/ApplicationConfig.java
@@ -23,13 +23,15 @@ public class ApplicationConfig extends Application {
     }
 
     /**
-     * Do not modify addRestResourceClasses() method. It is automatically
-     * populated with all resources defined in the project. If required, comment
-     * out calling this method in getClasses().
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(org.glassfish.jersey.client.filter.HttpDigestAuthFilter.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
-        resources.add(recursos.ConsultasResource.class);
+        resources.add(recursos.ConsultaResource.class);
     }
 
 }
