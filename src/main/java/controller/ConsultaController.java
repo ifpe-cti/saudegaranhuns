849973e085d262cf3fddc307d078dbc5b2a517b5
jofernando <fmdinho3@gmail.com>
2017-11-27 21:04:29 -0300

    comentando controller

--- src/main/java/controller/ConsultaController.java
+++ src/main/java/controller/ConsultaController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,15 +6,9 @@
 package controller;
 
 import java.io.Serializable;
-import java.util.List;
-import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
-import model.ConsultaModel;
-import model.entidades.Consulta;
-import model.entidades.Especialidade;
-import model.entidades.Prioridade;
+import javax.faces.bean.SessionScoped;
 
 /**
  *
@@ -25,114 +18,17 @@ import model.entidades.Prioridade;
 @SessionScoped
 public class ConsultaController implements Serializable {
 
-    private ConsultaModel model;
-    private Consulta consulta;
-
-    public Prioridade[] getPrioridade() {
-        return Prioridade.values();
-    }
-
-    public Especialidade[] getEspecialidade() {
-        return Especialidade.values();
+//    private ConsultaModel model;
+//    private Consulta consulta;
+    @PostConstruct
+    public void init() {
+//        consulta = new Consulta();
+//        model = new ConsultaModel();
     }
 
     public ConsultaController() {
-	consulta = new Consulta();
-	model = new ConsultaModel();
     }
 
-    public void cadastrar() {
-	if (model.cadastrar(consulta)) {
-	    FacesContext.getCurrentInstance().addMessage(
-		    null,
-		    new FacesMessage(
-			    FacesMessage.SEVERITY_INFO,
-			    "Sucesso, consulta cadastrada",
-			    null
-		    )
-	    );
-	}
-    }
-
-    public void alterar() {
-	if (model.alterar(consulta)) {
-	    FacesContext.getCurrentInstance().addMessage(
-		    null,
-		    new FacesMessage(
-			    FacesMessage.SEVERITY_INFO,
-			    "Sucesso, consulta alterada",
-			    null
-		    )
-	    );
-	}
-    }
-
-    public void cancelar() {
-	if (model.cancelar(consulta)) {
-	    FacesContext.getCurrentInstance().addMessage(
-		    null,
-		    new FacesMessage(
-			    FacesMessage.SEVERITY_INFO,
-			    "Sucesso, consulta cancelada",
-			    null
-		    )
-	    );
-	}
-    }
-
-    public Consulta buscar(int id) {
-        return model.buscar(id);
-    }
-
-    public List<Consulta> buscarTodos() {
-        return model.buscarTodos();
-    }
-
-    public Consulta getConsulta() {
-        return consulta;
-    }
-
-    public void setConsulta(Consulta consulta) {
-	this.consulta = consulta;
-    }
-
-}
-=======
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package controller;
-//
-//import java.io.Serializable;
-//import java.util.List;
-//import javax.annotation.PostConstruct;
-//import javax.faces.application.FacesMessage;
-//import javax.faces.bean.ManagedBean;
-//import javax.faces.bean.SessionScoped;
-//import javax.faces.context.FacesContext;
-//
-///**
-// *
-// * @author Fernando
-// */
-//@ManagedBean
-//@SessionScoped
-//public class ConsultaController implements Serializable {
-//
-//    private ConsultaModel model;
-//    private Consulta consulta;
-//
-//    @PostConstruct
-//    public void init() {
-//        consulta = new Consulta();
-//        model = new ConsultaModel();
-//    }
-//
-//    public ConsultaController() {
-//    }
-//
 //    public Prioridade[] getPrioridade() {
 //        return Prioridade.values();
 //    }
@@ -144,8 +40,7 @@ public class ConsultaController implements Serializable {
 //    public Status[] getStatus() {
 //        return Status.values();
 //    }
-//
-//    public void cadastrar() {
+    public void cadastrar() {
 //        if (model.cadastrar(consulta)) {
 //            FacesContext.getCurrentInstance().addMessage(
 //                    null,
@@ -157,9 +52,9 @@ public class ConsultaController implements Serializable {
 //            );
 //            limpar();
 //        }
-//    }
-//
-//    public void alterar() {
+    }
+
+    public void alterar() {
 //        if (model.alterar(consulta)) {
 //            FacesContext.getCurrentInstance().addMessage(
 //                    null,
@@ -170,9 +65,9 @@ public class ConsultaController implements Serializable {
 //                    )
 //            );
 //        }
-//    }
-//
-//    public void cancelar() {
+    }
+
+    public void cancelar() {
 //        if (model.cancelar(consulta)) {
 //            FacesContext.getCurrentInstance().addMessage(
 //                    null,
@@ -183,9 +78,9 @@ public class ConsultaController implements Serializable {
 //                    )
 //            );
 //        }
-//    }
-//
-//    public void cancelar(Consulta consulta) {
+    }
+
+    public void cancelar(int a/*Consulta consulta*/) {
 //        if (model.cancelar(consulta)) {
 //            FacesContext.getCurrentInstance().addMessage(
 //                    null,
@@ -196,8 +91,8 @@ public class ConsultaController implements Serializable {
 //                    )
 //            );
 //        }
-//    }
-//
+    }
+
 //    public Consulta buscar(int id) {
 //        return model.buscar(id);
 //    }
@@ -213,9 +108,7 @@ public class ConsultaController implements Serializable {
 //    public void setConsulta(Consulta consulta) {
 //        this.consulta = consulta;
 //    }
-//
-//    public void limpar() {
+    public void limpar() {
 //        this.consulta = new Consulta();
-//    }
-//}
->>>>>>> 11bb99a85df7b113df41b725393d488cf8465d5a
+    }
+}
