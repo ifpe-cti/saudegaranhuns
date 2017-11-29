04fc631fd5dde7c6308857e7bb49f80f64f78d73
José Fernando Mendes <fmdinho3@gmail.com>
2017-11-28 21:41:47 -0300

    Adicionando converter de posto de saude

---
+++ src/main/java/br/edu/ifpe/garanhuns/sg/converter/PostoSaudeConverter.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.garanhuns.sg.converter;
+
+import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.PostoSaudeHibernate;
+import br.edu.ifpe.garanhuns.sg.model.pojo.PostoSaude;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author 20161D13GR0155
+ */
+@FacesConverter("postoSaudeConverter")
+public class PostoSaudeConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext fc, UIComponent uic, String string) {
+        if (string != null && string.trim().length() > 0) {
+            try {
+                PostoSaudeHibernate hibernate = new PostoSaudeHibernate();
+                return hibernate.read(Integer.valueOf(string));
+            } catch (NumberFormatException e) {
+                System.err.println(e);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getAsString(FacesContext fc, UIComponent uic, Object o) {
+        if (o != null) {
+            try {
+                return String.valueOf(((PostoSaude) o).getId());
+            } catch (Exception ex) {
+            }
+        }
+        return null;
+    }
+
+}
