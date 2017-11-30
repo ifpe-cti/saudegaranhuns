b9d61eba902d7e9dc87658eea69eb79c259044ac
Fabricio Cabral <fabriciofx@gmail.com>
2017-11-30 19:45:20 -0300

    #16 Adicionado suporte ao SpringBoot com Tomcat embedded

---
+++ src/main/java/br/edu/ifpe/garanhuns/sg/Aplicacao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.garanhuns.sg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplicacao {
+    public static void main(String[] args) {
+        SpringApplication.run(Aplicacao.class, args);
+    }
+}
