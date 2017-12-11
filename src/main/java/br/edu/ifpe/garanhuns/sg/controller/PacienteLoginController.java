1f6ec1b08fe2abb987f579344fb9cc1371ed9854
Igor Silva de Sousa <igorr201@hotmail.com>
2017-12-11 20:08:28 -0300

    mudança de cadastro de paciente para retirada de endereço

---
+++ src/main/java/br/edu/ifpe/garanhuns/sg/controller/PacienteLoginController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.garanhuns.sg.controller;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Igor Sousa PC
+ */
+@ManagedBean
+@SessionScoped
+public class PacienteLoginController implements Serializable{
+    
+}
