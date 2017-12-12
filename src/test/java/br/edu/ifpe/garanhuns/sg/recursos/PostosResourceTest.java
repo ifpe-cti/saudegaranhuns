6a9c0139d8bfcdbcfc38f803b980bfeaa94c592f
José Junio <junio.araujo01@yahoo.com>
2017-12-12 18:36:22 -0300

    Adicionando classe de teste

---
+++ src/test/java/br/edu/ifpe/garanhuns/sg/recursos/PostosResourceTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.garanhuns.sg.recursos;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author Jose Junio
+ */
+public class PostosResourceTest {
+
+    /**
+     * Test of recuperarTodosOsPostos method, of class PostosResource.
+     */
+    @Before
+    public void removePostos() {
+
+    }
+
+    public void testRecuperarTodosOsPostos() {
+
+    }
+
+    @Test
+    public void testRecuperarListaDePostosVazia() {
+
+    }
+
+}
