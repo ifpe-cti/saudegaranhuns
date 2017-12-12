f782887fd56d4bc09c64097e70d32fdc4dcf500a
José Junio <junio.araujo01@yahoo.com>
2017-12-12 18:28:21 -0300

    Alterando anotações no serviço

--- src/main/java/br/edu/ifpe/garanhuns/sg/recursos/PostosResource.java
+++ src/main/java/br/edu/ifpe/garanhuns/sg/recursos/PostosResource.java
@@ -7,19 +7,20 @@ package br.edu.ifpe.garanhuns.sg.recursos;
 
 import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.PostoSaudeHibernate;
 import com.google.gson.Gson;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * REST Web Service
  *
  * @author Jose Junio
  */
-@Path("postos")
+@RestController
+@RequestMapping(value = "/postos")
 public class PostosResource {
 
     @Context
@@ -37,8 +38,7 @@ public class PostosResource {
      *
      * @return an instance of java.lang.String
      */
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
     public String recuperarTodosOsPostos() {
 
         Gson gson = new Gson();
