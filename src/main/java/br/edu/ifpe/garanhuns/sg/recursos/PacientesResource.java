87cea30cac65b668cc97f53ddc10a4145e4892ce
José Junio <junio.araujo01@yahoo.com>
2017-12-13 00:58:11 -0300

    Tratamento da resposta do POST ao cliente

--- src/main/java/br/edu/ifpe/garanhuns/sg/recursos/PacientesResource.java
+++ src/main/java/br/edu/ifpe/garanhuns/sg/recursos/PacientesResource.java
@@ -5,15 +5,15 @@
  */
 package br.edu.ifpe.garanhuns.sg.recursos;
 
-import br.edu.ifpe.garanhuns.sg.model.Endereco;
 import br.edu.ifpe.garanhuns.sg.model.Paciente;
-import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.EnderecoHibernate;
 import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.PacienteHibernate;
 import com.google.gson.Gson;
-import java.net.URI;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,17 +39,23 @@ public class PacientesResource {
     /**
      * Retrieves representation of an instance of recursos.PacientesResource
      *
+     * @param pacienteJson
      * @return an instance of java.lang.String
      */
     @RequestMapping(method = RequestMethod.POST)
-    public Response cadastrar(String pacienteJson) {
+    public ResponseEntity cadastrarPaciente(@RequestBody String pacienteJson) {
 
-        Paciente paciente = new Gson().fromJson(pacienteJson, Paciente.class);
-        Endereco enderecoPaciente = paciente.getEndereco();
-        new EnderecoHibernate().inserir(enderecoPaciente);
-        new PacienteHibernate().inserir(paciente);
-        //Alterar o caminho de resposta
-        URI uri = URI.create("/pacienteCadastrado");
-        return Response.created(uri).build();
+        if (Paciente.validarPacienteJson(pacienteJson)) {
+            Paciente paciente = new Gson().fromJson(pacienteJson, Paciente.class);
+            try {
+                new PacienteHibernate().inserir(paciente);
+                return ResponseEntity.status(HttpStatus.CREATED).build();
+            } catch (Exception err) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).build();
+        }
+
     }
 }
