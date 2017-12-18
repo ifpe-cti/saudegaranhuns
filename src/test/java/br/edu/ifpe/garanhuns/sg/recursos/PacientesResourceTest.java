741c7a30a8f03521f4291431784ed95dad95efdc
José Junio <junio.araujo01@yahoo.com>
2017-12-18 15:33:21 -0300

    Adicionando anotação @ignore nos testes

--- src/test/java/br/edu/ifpe/garanhuns/sg/recursos/PacientesResourceTest.java
+++ src/test/java/br/edu/ifpe/garanhuns/sg/recursos/PacientesResourceTest.java
@@ -8,11 +8,13 @@ package br.edu.ifpe.garanhuns.sg.recursos;
 import br.edu.ifpe.garanhuns.sg.model.Paciente;
 import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.PacienteHibernate;
 import java.net.URI;
-import javax.ws.rs.core.Response;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
@@ -21,14 +23,15 @@ import org.springframework.web.client.RestTemplate;
  *
  * @author Jose Junio
  */
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = PacientesResourceTest.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PacientesResourceTest.class)
 public class PacientesResourceTest {
 
     /**
      * Test of cadastrarPaciente method, of class PacientesResource.
      */
-    //@Test
+    @Ignore
+    @Test
     public void testCadastrarPacienteJsonValido() {
 
         Paciente paciente = new PacienteHibernate().recuperarPorNome("Teste");
@@ -48,7 +51,8 @@ public class PacientesResourceTest {
         new PacienteHibernate().deletar(p);
     }
 
-    //@Test
+    @Ignore
+    @Test
     public void testCadastrarPacienteJsonInvalido() {
         String pacienteJson = "\"id\":0,\"nome\":\"Teste\",\"cartaoSus\":\"321\",\"dataNascimento\":{\"year\":2010,\"month\":11,\"day\":10},"
                 + "\"endereco\":{\"id\":0,\"numero\":\"32\",\"logradouro\":\"Rua dos bobos\","
@@ -58,12 +62,17 @@ public class PacientesResourceTest {
                 + "\"bairro\":{\"id\":0,\"nome\":\"Juliana\"}}},"
                 + "\"usuario\":{\"id\":0,\"login\":\"login\",\"senha\":\"senha\",\"perfilUsuario\":\"ADMINISTRADOR\"}}";
         RestTemplate restTemplate = new RestTemplate();
+        System.out.println("===========================AQUII==================");
         ResponseEntity response = restTemplate.postForEntity(URI.create("http://localhost:8080/pacientes"), pacienteJson, String.class);
+        System.out.println("===============outro========================================");
+        System.out.println(response.getStatusCodeValue());
         Assert.assertTrue(response.getStatusCodeValue() == 415);
         Paciente p = new PacienteHibernate().recuperarPorNome("Teste");
         Assert.assertNull(p);
     }
 
+    @Ignore
+    @Test
     public void testCadastrarPacienteJsonCampoInvalido() {
         String pacienteJson = "{\"id\":0,\"ome\":\"Teste\",\"cartaoSus\":\"321\",\"dataNascimento\":{\"year\":2010,\"month\":11,\"day\":10},"
                 + "\"endereco\":{\"id\":0,\"numero\":\"32\",\"logradouro\":\"Rua dos bobos\","
@@ -72,9 +81,11 @@ public class PacientesResourceTest {
                 + "\"endereco\":{\"id\":0,\"numero\":\"32\",\"logradouro\":\"Rua dos bobos\","
                 + "\"bairro\":{\"id\":0,\"nome\":\"Juliana\"}}},"
                 + "\"usuario\":{\"id\":0,\"login\":\"login\",\"senha\":\"senha\",\"perfilUsuario\":\"ADMINISTRADOR\"}}";
+
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity response = restTemplate.postForEntity(URI.create("http://localhost:8080/pacientes"), pacienteJson, String.class);
-        Assert.assertTrue(response.getStatusCodeValue() == 415);
+        RequestEntity request = new RequestEntity(HttpMethod.POST, URI.create("http://localhost:8080/pacientes"));
+        ResponseEntity response = restTemplate.postForEntity("http://localhost:8080/pacientes", request, String.class);
+        Assert.assertEquals(response.getStatusCodeValue(), 415);
         Paciente p = new PacienteHibernate().recuperarPorNome("Teste");
         Assert.assertNull(p);
     }
