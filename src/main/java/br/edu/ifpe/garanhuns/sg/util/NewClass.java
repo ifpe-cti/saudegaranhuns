f23eb6e08e5a29a434592b7cff8cc2bddc48c8b8
Hérikles Vinicyus <heriklesvinicyus@hotmail.com>
2017-12-18 21:35:41 -0300

    #128 remoção de conflito 3

--- src/main/java/br/edu/ifpe/garanhuns/sg/util/NewClass.java
+++ src/main/java/br/edu/ifpe/garanhuns/sg/util/NewClass.java
@@ -34,8 +34,8 @@ public class NewClass {
         ConsultaHibernate cH = new ConsultaHibernate();
         Endereco e = new Endereco("32", "Rua dos bobos", new Bairro("Juliana"));
         //
-        Paciente p;
-        //p = new Paciente("asdf", "321", LocalDate.of(2010, 11, 10), e, new PostoSaude("asd", e), new Usuario("login", "senha", PerfilUsuario.ADMINISTRADOR));
+        /*Paciente p;
+        p = new Paciente("asdf", "321", LocalDate.of(2010, 11, 10), e, new PostoSaude("asd", e), new Usuario("login", "senha", PerfilUsuario.ADMINISTRADOR));
         Consulta c = new Consulta(Especialidade.GERAL, Prioridade.IDOSO, Status.FILA, LocalDate.of(2017, 11, 24), LocalDate.of(2018, 1, 1), p);
 
         eh.inserir(e);
@@ -43,7 +43,7 @@ public class NewClass {
         cH.inserir(c);
         System.out.println("%%%%%%%%%%%%%%%%%%%" + ph.recuperarPorCartaoSus("321"));
         cH.alterarStatusConsulta(c, 2);
-        ph.recuperarPorCartaoSus("321");
+        ph.recuperarPorCartaoSus("321");*/
 
     }
 }
