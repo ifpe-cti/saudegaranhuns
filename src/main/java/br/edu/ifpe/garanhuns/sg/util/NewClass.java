93c82f97bbc9b50dfafb263440adf06c320453bc
Hérikles Vinicyus <heriklesvinicyus@hotmail.com>
2017-12-18 21:35:00 -0300

    #128 remoção de conflito 2

--- src/main/java/br/edu/ifpe/garanhuns/sg/util/NewClass.java
+++ src/main/java/br/edu/ifpe/garanhuns/sg/util/NewClass.java
@@ -24,10 +24,26 @@ import br.edu.ifpe.garanhuns.sg.model.enumarador.Status;
  *
  * @author nosso
  */
+
+
 public class NewClass {
 
     public static void main(String[] args) {
-        
+        EnderecoHibernate eh = new EnderecoHibernate();
+        PacienteHibernate ph = new PacienteHibernate();
+        ConsultaHibernate cH = new ConsultaHibernate();
+        Endereco e = new Endereco("32", "Rua dos bobos", new Bairro("Juliana"));
+        //
+        Paciente p;
+        //p = new Paciente("asdf", "321", LocalDate.of(2010, 11, 10), e, new PostoSaude("asd", e), new Usuario("login", "senha", PerfilUsuario.ADMINISTRADOR));
+        Consulta c = new Consulta(Especialidade.GERAL, Prioridade.IDOSO, Status.FILA, LocalDate.of(2017, 11, 24), LocalDate.of(2018, 1, 1), p);
+
+        eh.inserir(e);
+        ph.inserir(p);
+        cH.inserir(c);
+        System.out.println("%%%%%%%%%%%%%%%%%%%" + ph.recuperarPorCartaoSus("321"));
+        cH.alterarStatusConsulta(c, 2);
+        ph.recuperarPorCartaoSus("321");
 
     }
 }
