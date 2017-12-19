1166b47865a21df92b9d714fe3ad3a027b0023ab
Hérikles Vinicyus <heriklesvinicyus@hotmail.com>
2017-12-18 21:46:56 -0300

    #128 remoção de conflito 5

--- src/main/java/br/edu/ifpe/garanhuns/sg/util/NewClass.java
+++ src/main/java/br/edu/ifpe/garanhuns/sg/util/NewClass.java
@@ -5,21 +5,6 @@
  */
 package br.edu.ifpe.garanhuns.sg.util;
 
-import java.time.LocalDate;
-import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.ConsultaHibernate;
-import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.EnderecoHibernate;
-import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.PacienteHibernate;
-import br.edu.ifpe.garanhuns.sg.model.Bairro;
-import br.edu.ifpe.garanhuns.sg.model.Consulta;
-import br.edu.ifpe.garanhuns.sg.model.Endereco;
-import br.edu.ifpe.garanhuns.sg.model.Paciente;
-import br.edu.ifpe.garanhuns.sg.model.PostoSaude;
-import br.edu.ifpe.garanhuns.sg.model.Usuario;
-import br.edu.ifpe.garanhuns.sg.model.enumarador.Especialidade;
-import br.edu.ifpe.garanhuns.sg.model.enumarador.PerfilUsuario;
-import br.edu.ifpe.garanhuns.sg.model.enumarador.Prioridade;
-import br.edu.ifpe.garanhuns.sg.model.enumarador.Status;
-
 /**
  *
  * @author nosso
@@ -29,21 +14,6 @@ import br.edu.ifpe.garanhuns.sg.model.enumarador.Status;
 public class NewClass {
 
     public static void main(String[] args) {
-        EnderecoHibernate eh = new EnderecoHibernate();
-        PacienteHibernate ph = new PacienteHibernate();
-        ConsultaHibernate cH = new ConsultaHibernate();
-        Endereco e = new Endereco("32", "Rua dos bobos", new Bairro("Juliana"));
-        //
-        Paciente p;
-        p = new Paciente("asdf", "321", LocalDate.of(2010, 11, 10), e, new PostoSaude("asd", e), new Usuario("login", "senha", PerfilUsuario.ADMINISTRADOR));
-        Consulta c = new Consulta(Especialidade.GERAL, Prioridade.IDOSO, Status.FILA, LocalDate.of(2017, 11, 24), LocalDate.of(2018, 1, 1), p);
-
-        eh.inserir(e);
-        ph.inserir(p);
-        cH.inserir(c);
-        System.out.println("%%%%%%%%%%%%%%%%%%%" + ph.recuperarPorCartaoSus("321"));
-        cH.alterarStatusConsulta(c, 2);
-        ph.recuperarPorCartaoSus("321");*/
 
     }
 }
