911fb757d67057a2a03b069b97e9ac7469968b39
Igor Silva de Sousa <igorr201@hotmail.com>
2017-12-20 23:49:26 -0300

    #140 Aplicando metodo de login

--- src/main/java/br/edu/ifpe/garanhuns/sg/controller/LoginUsuarioController.java
+++ src/main/java/br/edu/ifpe/garanhuns/sg/controller/LoginUsuarioController.java
@@ -9,11 +9,14 @@ import br.edu.ifpe.garanhuns.sg.model.Atendente;
 import br.edu.ifpe.garanhuns.sg.model.Paciente;
 import br.edu.ifpe.garanhuns.sg.model.Usuario;
 import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.PacienteHibernate;
+import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.UsuarioHibernate;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Map;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
 /**
@@ -24,59 +27,27 @@ import javax.faces.context.FacesContext;
 @SessionScoped
 public class LoginUsuarioController {
 
-    private Usuario userLogado = null;
-    private Usuario usuario = new Usuario();
+    private UsuarioHibernate usuarioh = new UsuarioHibernate();
 
     public LoginUsuarioController() {
     }
-    
+
     public String realizarLogin(String login, String senha) {
-        
-        PacienteController paciente = new PacienteController();
-        AtendenteController atendente = new AtendenteController();
-        ArrayList<Paciente> pacientes = (ArrayList<Paciente>) paciente.recuperarTodos();
-        ArrayList<Atendente> atendentes = (ArrayList<Atendente>) atendente.recuperarTodos();
-        Usuario usuario = new Usuario();
-        
-        for (int i = 0; i < pacientes.size(); i++) {
-            
-            if (pacientes.get(i).getUsuario().getLogin().equals(login) && pacientes.get(i).getUsuario().getSenha().equals(senha)) {
-                userLogado = usuario;
-                break;
-            }
-        }
-        for (int i = 0; i < atendentes.size(); i++) {
-            
-            if (atendentes.get(i).getUsuario().getLogin().equals(login) && atendentes.get(i).getUsuario().getSenha().equals(senha)) {
-                userLogado = usuario;
-                break;
-            }
-        }
-        
-        if ((userLogado == null) ) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Problema!", "O usuário não existe!"));
-            return "index.html";
-        }
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Usuário logado com sucesso!"));
-            
-                    
-            return "index.html";
-    }
-    public String realizarlogout(){
-        
-        userLogado = null;
-        
-        return "index.html?faces-redirect=true";
-    }
+        FacesContext context = FacesContext.getCurrentInstance();
 
-    public Usuario getUserLogado() {
-        return userLogado;
-    }
+        Usuario usuarioLogado = usuarioh.recuperarUsuarioPorLogin(login, senha);
 
-    public void setUserLogado(Usuario userLogado) {
-        this.userLogado = userLogado;
-    }
-        
- 
+        if (usuarioLogado != null) {
+            ExternalContext externalContext = context.getExternalContext();
+            Map<String, Object> session = externalContext.getSessionMap();
 
+            session.put("usuarioLogado", usuarioLogado);
+            context.addMessage(null, new FacesMessage("Sucesso", "Login realizado com sucesso!"));
+            return "/index.xhtml";
+        } else {
+
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro!", "Login ou senha inválidos."));
+        }
+        return "";
+    }
 }
