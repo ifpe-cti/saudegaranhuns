72d13a2188aed2c6e6e31a1a71d7840c466c1fee
José Fernando Mendes <fmdinho3@gmail.com>
2017-12-23 07:16:28 -0300

    Adicionando métodos para realizar logout e retornar o tipo de usuario logado

--- src/main/java/br/edu/ifpe/garanhuns/sg/controller/LoginUsuarioController.java
+++ src/main/java/br/edu/ifpe/garanhuns/sg/controller/LoginUsuarioController.java
@@ -5,19 +5,15 @@
  */
 package br.edu.ifpe.garanhuns.sg.controller;
 
-import br.edu.ifpe.garanhuns.sg.model.Atendente;
-import br.edu.ifpe.garanhuns.sg.model.Paciente;
 import br.edu.ifpe.garanhuns.sg.model.Usuario;
-import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.PacienteHibernate;
 import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.UsuarioHibernate;
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Map;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -34,20 +30,26 @@ public class LoginUsuarioController {
 
     public String realizarLogin(String login, String senha) {
         FacesContext context = FacesContext.getCurrentInstance();
-
         Usuario usuarioLogado = usuarioh.recuperarUsuarioPorLogin(login, senha);
-
         if (usuarioLogado != null) {
             ExternalContext externalContext = context.getExternalContext();
             Map<String, Object> session = externalContext.getSessionMap();
-
             session.put("usuarioLogado", usuarioLogado);
             context.addMessage(null, new FacesMessage("Sucesso", "Login realizado com sucesso!"));
-            return "/index.xhtml";
+            return "index.xhtml";
         } else {
-
             context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro!", "Login ou senha inválidos."));
+            return null;
         }
-        return "";
+    }
+
+    public String realizarLogout() {
+        ((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true)).invalidate();
+        return "index.xhtml";
+    }
+
+    public String tipoUsuarioLogado() {
+        Usuario usuario = (Usuario) ((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true)).getAttribute("usuarioLogado");
+        return usuario.getPerfilUsuario().getNome();
     }
 }
