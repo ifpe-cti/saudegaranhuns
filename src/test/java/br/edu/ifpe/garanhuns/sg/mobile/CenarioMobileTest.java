8aae424ff3031d9aa07b25d8be42e7252fa7daa1
José Junio <junio.araujo01@yahoo.com>
2017-12-26 00:18:25 -0300

    Criação do cenário de teste para versão mobile

---
+++ src/test/java/br/edu/ifpe/garanhuns/sg/mobile/CenarioMobileTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.garanhuns.sg.mobile;
+
+import br.edu.ifpe.garanhuns.sg.model.Atendimento;
+import br.edu.ifpe.garanhuns.sg.model.Bairro;
+import br.edu.ifpe.garanhuns.sg.model.Endereco;
+import br.edu.ifpe.garanhuns.sg.model.HorarioAtendimento;
+import br.edu.ifpe.garanhuns.sg.model.PostoSaude;
+import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.HorarioAtendimentoHibernate;
+import br.edu.ifpe.garanhuns.sg.model.enumarador.DiasSemana;
+import br.edu.ifpe.garanhuns.sg.model.enumarador.Especialidade;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Jose Junio
+ */
+public class CenarioMobileTest {
+
+    @Test
+    public void testIniciarUsoMobile() {
+        Bairro bairro = new Bairro("Centro");
+        Endereco endereco = new Endereco("007", "Rua Aqui", bairro);
+        PostoSaude posto = new PostoSaude("Posto Teste", endereco);
+        Atendimento atendimentoGeral = new Atendimento(Especialidade.GERAL, posto);
+        Atendimento atendimentoDentista = new Atendimento(Especialidade.DENTISTA, posto);
+
+        HorarioAtendimento horarioAtendimentoGeral = new HorarioAtendimento(DiasSemana.SEGUNDA, "07:00", "17:00", 20, atendimentoGeral);
+        HorarioAtendimento horarioAtendimentoDentista = new HorarioAtendimento(DiasSemana.TERÇA, "07:00", "17:00", 20, atendimentoDentista);
+
+        new HorarioAtendimentoHibernate().inserir(horarioAtendimentoGeral);
+        new HorarioAtendimentoHibernate().inserir(horarioAtendimentoDentista);
+        List<HorarioAtendimento> horarioBDGeral = new HorarioAtendimentoHibernate().
+                recuperarHorarioAtendimentoPorPostoSaudeEspecialidade(posto, Especialidade.GERAL);
+        List<HorarioAtendimento> horarioBDDentista = new HorarioAtendimentoHibernate().
+                recuperarHorarioAtendimentoPorPostoSaudeEspecialidade(posto, Especialidade.DENTISTA);
+
+        Assert.assertEquals(1, horarioBDDentista.size());
+        Assert.assertEquals(1, horarioBDGeral.size());
+
+    }
+
+}
