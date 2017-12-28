1dc05a9b68e02551b95f46ddbc89b2c94aa65017
José Junio <junio.araujo01@yahoo.com>
2017-12-28 01:15:20 -0300

    Adicionando as classes de testes

---
+++ src/test/java/br/edu/ifpe/garanhuns/sg/model/PacienteTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.garanhuns.sg.model;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Jose Junio
+ */
+public class PacienteTest {
+
+    @Test
+    public void testValidarPacienteJson() {
+
+    }
+
+}
