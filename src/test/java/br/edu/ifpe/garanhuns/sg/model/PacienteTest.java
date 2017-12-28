9edcda4116b181c7eaf5444a8769100eea3585af
José Junio <junio.araujo01@yahoo.com>
2017-12-28 01:36:04 -0300

    #200 inclusão de testes unitários

--- src/test/java/br/edu/ifpe/garanhuns/sg/model/PacienteTest.java
+++ src/test/java/br/edu/ifpe/garanhuns/sg/model/PacienteTest.java
@@ -5,6 +5,10 @@
  */
 package br.edu.ifpe.garanhuns.sg.model;
 
+import br.edu.ifpe.garanhuns.sg.model.enumarador.PerfilUsuario;
+import com.google.gson.Gson;
+import java.time.LocalDate;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -13,9 +17,38 @@ import org.junit.Test;
  */
 public class PacienteTest {
 
+    /**
+     * Test of validarPacienteJson method, of class Paciente.
+     */
     @Test
-    public void testValidarPacienteJson() {
+    public void testValidarPacienteJsonValido() {
+        String pacienteJson = "{\"id\":0,\"nome\":\"Teste\",\"cartaoSus\":\"123\",\"dataNascimento\":{\"year\":2017,\"month\":12,\"day\":28},"
+                + "\"postoSaude\":{\"id\":0,\"nome\":\"Posto\","
+                + "\"endereco\":{\"id\":0,\"numero\":\"123\",\"logradouro\":\"Rua\","
+                + "\"bairro\":{\"id\":0,\"nome\":\"Centro\"}}},"
+                + "\"usuario\":{\"id\":0,\"login\":\"LoginTest\",\"senha\":\"000009\",\"perfilUsuario\":\"PACIENTE\"}}";
+        Assert.assertTrue(Paciente.validarPacienteJson(pacienteJson));
+    }
 
+    @Test
+    public void testValidarPacienteJsonFormatoInvalido() {
+        String pacienteJson = "\"id\":0,\"nome\":\"Teste\",\"cartaoSus\":\"123\",\"dataNascimento\":{\"year\":2017,\"month\":12,\"day\":28},"
+                + "\"postoSaude\":{\"id\":0,\"nome\":\"Posto\","
+                + "\"endereco\":{\"id\":0,\"numero\":\"123\",\"logradouro\":\"Rua\","
+                + "\"bairro\":{\"id\":0,\"nome\":\"Centro\"}}},"
+                + "\"usuario\":{\"id\":0,\"login\":\"LoginTest\",\"senha\":\"000009\",\"perfilUsuario\":\"PACIENTE\"}}";
+        Assert.assertFalse(Paciente.validarPacienteJson(pacienteJson));
+
+    }
+
+    @Test
+    public void testValidarPacienteJsonCampoNomeInvalido() {
+        String pacienteJson = "{\"id\":0,\"nom\":\"Teste\",\"cartaoSus\":\"123\",\"dataNascimento\":{\"year\":2017,\"month\":12,\"day\":28},"
+                + "\"postoSaude\":{\"id\":0,\"nome\":\"Posto\","
+                + "\"endereco\":{\"id\":0,\"numero\":\"123\",\"logradouro\":\"Rua\","
+                + "\"bairro\":{\"id\":0,\"nome\":\"Centro\"}}},"
+                + "\"usuario\":{\"id\":0,\"login\":\"LoginTest\",\"senha\":\"000009\",\"perfilUsuario\":\"PACIENTE\"}}";
+        Assert.assertFalse(Paciente.validarPacienteJson(pacienteJson));
     }
 
 }
