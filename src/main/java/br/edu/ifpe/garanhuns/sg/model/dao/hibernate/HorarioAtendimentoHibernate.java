fe221f60cdd328284c2686fc86003fc7f0712ad4
Herikles Vinicyus Franca Cordeiro <20161d13gr0082@LABS.GARANHUNS.IFPE>
2017-12-28 15:00:10 -0300

    #206 Reorganização dos imports de HorarioAtendimentoHibernate

--- src/main/java/br/edu/ifpe/garanhuns/sg/model/dao/hibernate/HorarioAtendimentoHibernate.java
+++ src/main/java/br/edu/ifpe/garanhuns/sg/model/dao/hibernate/HorarioAtendimentoHibernate.java
@@ -5,6 +5,7 @@
  */
 package br.edu.ifpe.garanhuns.sg.model.dao.hibernate;
 
+import br.edu.ifpe.garanhuns.sg.model.DiasSemana;
 import java.util.List;
 import br.edu.ifpe.garanhuns.sg.model.dao.HorarioAtendimentoDAO;
 import br.edu.ifpe.garanhuns.sg.model.HorarioAtendimento;
