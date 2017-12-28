eab7ef314da089dd0441c066aa669a0c7dc6b737
Hérikles Vinícyus <heriklesvinicyus@hotmail.com>
2017-12-28 02:08:08 -0300

    #192 Remoção do pacote interface e movendo os DAO's para o pacote DAO

--- src/main/java/br/edu/ifpe/garanhuns/sg/model/dao/PacienteDAO.java
+++ src/main/java/br/edu/ifpe/garanhuns/sg/model/dao/PacienteDAO.java
@@ -8,7 +8,7 @@ package br.edu.ifpe.garanhuns.sg.model.dao;
 import br.edu.ifpe.garanhuns.sg.model.Paciente;
 
 /**
- *
+ * 
  * @author Herikles
  */
 public interface PacienteDAO extends DAO<Paciente> {
