9a1380b548ae7d788ba5204807e88c486fcf0528
José Junio <junio.araujo01@yahoo.com>
2018-02-14 19:50:50 -0300

    Criada função para criar o paciente no formato Json

--- src/test/java/br/edu/ifpe/garanhuns/sg/recursos/PacientesResourceTest.java
+++ src/test/java/br/edu/ifpe/garanhuns/sg/recursos/PacientesResourceTest.java
@@ -5,9 +5,16 @@
  */
 package br.edu.ifpe.garanhuns.sg.recursos;
 
+import br.edu.ifpe.garanhuns.sg.model.Bairro;
+import br.edu.ifpe.garanhuns.sg.model.Endereco;
 import br.edu.ifpe.garanhuns.sg.model.Paciente;
+import br.edu.ifpe.garanhuns.sg.model.PerfilUsuario;
+import br.edu.ifpe.garanhuns.sg.model.PostoSaude;
+import br.edu.ifpe.garanhuns.sg.model.Usuario;
 import br.edu.ifpe.garanhuns.sg.model.dao.hibernate.PacienteHibernate;
+import com.google.gson.Gson;
 import java.net.URI;
+import java.time.LocalDate;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,29 +44,30 @@ public class PacientesResourceTest {
     @LocalServerPort
     int port;
 
+    public String gerarPacienteJson() {
+
+        Bairro bairroTeste = new Bairro("Centro");
+        Endereco enderecoTeste = new Endereco("001", "Rua", bairroTeste);
+        PostoSaude postoTeste = new PostoSaude("Posto Teste", enderecoTeste);
+        Usuario usuarioTeste = new Usuario("login", "senha", PerfilUsuario.PACIENTE);
+        Paciente pacienteTeste = new Paciente("Paciente Teste", "1234 5678", LocalDate.now(), postoTeste, usuarioTeste);
+        return new Gson().toJson(pacienteTeste);
+    }
+
     @Test
     public void testCadastrarPacienteJsonValido() {
 
-        Paciente paciente = new PacienteHibernate().recuperarPorNome("PacienteTeste");
-        Assert.assertNull(paciente);
-        String pacienteJson = "{\"id\":0,\"nome\":\"PacienteTeste\",\"cartaoSus\":\"321\",\"dataNascimento\":{\"year\":2010,\"month\":11,\"day\":10},"
-                + "\"endereco\":{\"id\":0,\"numero\":\"32\",\"logradouro\":\"Rua dos bobos\","
-                + "\"bairro\":{\"id\":0,\"nome\":\"Juliana\"}},"
-                + "\"postoSaude\":{\"id\":0,\"nome\":\"PSF\","
-                + "\"endereco\":{\"id\":0,\"numero\":\"32\",\"logradouro\":\"Rua dos bobos\","
-                + "\"bairro\":{\"id\":0,\"nome\":\"Juliana\"}}},"
-                + "\"usuario\":{\"id\":0,\"login\":\"login\",\"senha\":\"senha\",\"perfilUsuario\":\"ADMINISTRADOR\"}}";
-
-        RequestEntity request = new RequestEntity(pacienteJson, HttpMethod.POST, URI.create("http://localhost:" + port + "/pacientes"));
+        Paciente p = new PacienteHibernate().recuperarPorNome("Paciente Teste");
+        RequestEntity request = new RequestEntity(gerarPacienteJson(), HttpMethod.POST, URI.create("http://localhost:" + port + "/pacientes"));
         ResponseEntity response = this.restTemplate.postForEntity("http://localhost:" + port + "/pacientes", request, String.class);
         Assert.assertTrue(response.getStatusCodeValue() == 201);
-        Paciente p = new PacienteHibernate().recuperarPorNome("PacienteTeste");
-        //Assert.assertEquals("PacienteTeste", p.getNome());
         new PacienteHibernate().deletar(p);
+
     }
 
     @Test
     public void testCadastrarPacienteJsonInvalido() {
+
         String pacienteJson = "\"id\":0,\"nome\":\"PacienteTeste\",\"cartaoSus\":\"321\",\"dataNascimento\":{\"year\":2010,\"month\":11,\"day\":10},"
                 + "\"endereco\":{\"id\":0,\"numero\":\"32\",\"logradouro\":\"Rua dos bobos\","
                 + "\"bairro\":{\"id\":0,\"nome\":\"Juliana\"}},"
@@ -76,6 +84,7 @@ public class PacientesResourceTest {
 
     @Test
     public void testCadastrarPacienteJsonCampoInvalido() {
+
         String pacienteJson = "{\"id\":0,\"ome\":\"PacienteTeste\",\"cartaoSus\":\"321\",\"dataNascimento\":{\"year\":2010,\"month\":11,\"day\":10},"
                 + "\"endereco\":{\"id\":0,\"numero\":\"32\",\"logradouro\":\"Rua dos bobos\","
                 + "\"bairro\":{\"id\":0,\"nome\":\"Juliana\"}},"
